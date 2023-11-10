77dd9529b1e3910ce6dcc63b62e5beb707c3b876
yavaka <yavakasolutions@gmail.com>
2023-11-10 21:42:12 +0000

    MTMA-55 Setup Blazor WASM project for admin panel

---
+++ MTMA.AdminPanel/MTMA.AdminPanel/Services/Common/SessionStorage/ISessionStorageAccessor.cs
@@ -0,0 +1,28 @@
+﻿namespace MTMA.AdminPanel.Services.Common.SessionStorage
+{
+    /// <summary>
+    /// Represents an accessor for interacting with session storage in a Blazor WebAssembly application.
+    /// </summary>
+    public interface ISessionStorageAccessor
+    {
+        /// <summary>
+        /// Gets the value associated with the specified key from session storage.
+        /// </summary>
+        Task<T> GetValueAsync<T>(string key);
+        
+        /// <summary>
+        /// Sets the value for the specified key in session storage.
+        /// </summary>
+        Task SetValueAsync<T>(string key, T value);
+        
+        /// <summary>
+        /// Clears all key-value pairs from session storage.
+        /// </summary>
+        Task Clear();
+        
+        /// <summary>
+        /// Clears all key-value pairs from session storage.
+        /// </summary>
+        Task RemoveAsync(string key);
+    }
+}
---
+++ MTMA.AdminPanel/MTMA.AdminPanel/Services/Common/SessionStorage/SessionStorageAccessor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.JSInterop;
+
+namespace MTMA.AdminPanel.Services.Common.SessionStorage
+{
+    public class SessionStorageAccessor : ISessionStorageAccessor, IAsyncDisposable
+    {
+        private Lazy<IJSObjectReference> _accessorJsRef = new();
+        private readonly IJSRuntime _jsRuntime;
+
+        public SessionStorageAccessor(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task<T> GetValueAsync<T>(string key)
+        {
+            await WaitForReference();
+            var result = await _accessorJsRef.Value.InvokeAsync<T>("get", key);
+
+            return result;
+        }
+
+        public async Task SetValueAsync<T>(string key, T value)
+        {
+            await WaitForReference();
+            await _accessorJsRef.Value.InvokeVoidAsync("set", key, value);
+        }
+
+        public async Task Clear()
+        {
+            await WaitForReference();
+            await _accessorJsRef.Value.InvokeVoidAsync("clear");
+        }
+
+        
+        public async Task RemoveAsync(string key)
+        {
+            await WaitForReference();
+            await _accessorJsRef.Value.InvokeVoidAsync("remove", key);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_accessorJsRef.IsValueCreated)
+            {
+                await _accessorJsRef.Value.DisposeAsync();
+            }
+        }
+
+        #region Helpers
+
+        /// <summary>
+        /// Ensures that the JavaScript object reference is available before performing operations.
+        /// </summary>
+        /// <returns></returns>
+        private async Task WaitForReference()
+        {
+            if (_accessorJsRef.IsValueCreated is false)
+            {
+                _accessorJsRef = new(await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/SessionStorageAccessor.js"));
+            }
+        }
+
+        #endregion
+    }
+}
